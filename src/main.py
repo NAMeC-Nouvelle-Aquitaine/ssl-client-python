42db412f120d6aa1d34beabd774858a87515c422
MENIER Thomas Wanchaï <wanchai.menier@gmail.com>
2023-01-02 20:29:57 +0100

    modif: main.py makes rob blue 0 go around field weird deviation happening when using .control() method on grSim we should probably investigate

--- src/main.py
+++ src/main.py
@@ -2,19 +2,82 @@ import time
 
 import numpy as np
 
-from client import Client
+from client import Client, ClientRobot
+
+team = 'blue'
+rob_id = 0
+
+
+def angle_towards(src: tuple[float, float], dst: tuple[float, float]) -> float:
+    """
+    Returns the angle in radian from one point towards another one point
+    """
+    return np.arctan2(
+        dst[1] - src[1],
+        dst[0] - src[0]
+    )
+
+
+def stop_robot(rob: ClientRobot):
+    for _ in range(10):
+        rob.control(0., 0., 0.)
+
+
+def get_field_scale(rob: ClientRobot) -> int:
+    return 1000 if (abs(rob.position[0]) > 6.5 or abs(rob.position[1] > 4.5)) else 1
+
+
+def slow_goto(rob: ClientRobot, pos: tuple[float, float], tol: float):
+    # orienting towards destination point
+    theta = angle_towards(rob.position, pos)
+    rob.goto((rob.position[0], rob.position[1], theta))
+
+    # roomba towards point, slowly
+    while not np.allclose(rob.position, pos, atol=tol):
+        rob.control(0.5, 0, 0)
+    stop_robot(rob)
+    rob.goto((pos[0], pos[1], theta))
+    print(f"Attained {pos}")
+
+
+def you_make_my_head_go_round_right_round(rob: ClientRobot):
+    rob.goto((0, 0, 0))
+    for _ in range(10):
+        rob.control(0, 0, 10)
+    stop_robot(rob)
+    for _ in range(10):
+        rob.control(0, 0, -10)
+    stop_robot(rob)
+    rob.goto((0, 0, 0))
+
+
+def run_around_field_celebration(rob: ClientRobot, scale: int, tolerance: float):
+    """
+    Makes a given robot go around the field
+
+    Note : testing this on grSim made the robots deviate using the .control() method
+           a bit weird that this happened.
+           If this happens IRL please change the 'tolerance' parameter to something higher
+    """
+
+    top_mid = (0., 4.5*scale)
+    top_left = (-6*scale, 4.5*scale)
+    bottom_left = (-6*scale, -4.5*scale)
+    bottom_right = (6*scale, -4.5*scale)
+    top_right = (6*scale, 4.5*scale)
+
+    # navigating
+    slow_goto(rob, top_mid, tolerance)
+    slow_goto(rob, top_left, tolerance)
+    slow_goto(rob, bottom_left, tolerance)
+    slow_goto(rob, bottom_right, tolerance)
+    slow_goto(rob, top_right, tolerance)
+    slow_goto(rob, (0., 0.), tolerance)
+
 
 with Client(host='127.0.0.1', key='') as client:
-    robot = client.robots["blue"][0]
-    print(client.ball)
-    print(robot)
-    robot.goto(lambda: (0., 0., 0.), wait=True)
-    robot.dribble(0.0)
-
-    while True:
-        if robot.infrared:
-            robot.dribble(1000.0)
-            robot.goto((np.cos(time.time()) * 1000, np.sin(time.time()) * 1000, .0), wait=False)
-        else:
-            robot.dribble(0.0)
-            robot.control(.0, .0, .0)
+    robot = client.robots[team][rob_id]
+    s = get_field_scale(robot)
+    print(f"Scale = {s}")
+    # you_make_my_head_go_right_round(robot)
+    run_around_field_celebration(robot, s, tolerance=2.5*s)
