83472ccc23f6ecf33ea179f27ecf04febab4feeb
Léo Paillé <leo.paille@etu.u-bordeaux.fr>
2023-01-04 21:24:02 +0100

    working bezier

--- src/main.py
+++ src/main.py
@@ -8,13 +8,21 @@ with Client(host='127.0.0.1', key='') as client:
     robot = client.robots["blue"][0]
     print(client.ball)
     print(robot)
-    robot.goto(lambda: (0., 0., 0.), wait=True)
-    robot.dribble(0.0)
+    # robot.goto(lambda: (0., 0., 0.), wait=True)
 
-    while True:
-        if robot.infrared:
-            robot.dribble(1000.0)
-            robot.goto((np.cos(time.time()) * 1000, np.sin(time.time()) * 1000, .0), wait=False)
-        else:
-            robot.dribble(0.0)
-            robot.control(.0, .0, .0)
+    p0 = robot.position
+    p1 = np.array([2., 2.])
+    p2 = np.array([0., 0.])
+
+    p0_to_p1 = p1 - p0
+    p1_to_p2 = p2 - p1
+
+    for lerp in np.linspace(0, 1, 100):
+        P1 = p0 + p0_to_p1 * lerp
+        P2 = p1 + p1_to_p2 * lerp
+
+        P1_to_P2 = P2 - P1
+
+        p = P1 + P1_to_P2 * lerp
+        print(p)
+        robot.goto((*p, 0))
