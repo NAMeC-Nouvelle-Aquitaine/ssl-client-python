7225f52c61991fadf0e3c5f4114583bc6f7bf871
Léo Paillé <leo.paille@etu.u-bordeaux.fr>
2023-01-09 22:55:57 +0100

    added rrt

--- src/main.py
+++ src/main.py
@@ -3,26 +3,37 @@ import time
 import numpy as np
 
 from client import Client
+from src.rrt import RRT_star, dijkstra, plot
 
 with Client(host='127.0.0.1', key='') as client:
     robot = client.robots["blue"][0]
     print(client.ball)
     print(robot)
-    # robot.goto(lambda: (0., 0., 0.), wait=True)
+    # robot.goto(lambda: (-2., 0., 0.), wait=True)
 
-    p0 = robot.position
-    p1 = np.array([2., 2.])
-    p2 = np.array([0., 0.])
+    startpos = (robot.position[0], robot.position[1])
+    endpos = (2.0, 0.0)
+    obstacles = []
+    for r in client.robots["blue"].values():
+        if r != robot:
+            obstacles.append((r.position[0], r.position[1]))
+    for r in client.robots["yellow"].values():
+        if r != robot:
+            obstacles.append((r.position[0], r.position[1]))
 
-    p0_to_p1 = p1 - p0
-    p1_to_p2 = p2 - p1
+    n_iter = 600
+    radius = 0.2
+    stepSize = 0.3
 
-    for lerp in np.linspace(0, 1, 100):
-        P1 = p0 + p0_to_p1 * lerp
-        P2 = p1 + p1_to_p2 * lerp
+    G = RRT_star(startpos, endpos, obstacles, n_iter, radius, stepSize)
 
-        P1_to_P2 = P2 - P1
+    if G.success:
+        path = dijkstra(G)
+        print(path)
 
-        p = P1 + P1_to_P2 * lerp
-        print(p)
-        robot.goto((*p, 0))
+        plot(G, obstacles, radius, path)
+        for p in path:
+            robot.goto((*p, 0), wait=True)
+
+    else:
+        plot(G, obstacles, radius)
